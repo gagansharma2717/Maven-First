75a13773f03ae6993caceebcf2afa88f88393f25
sharmagagan276 <sharmagagan276@gmail.com>
2021-05-08 21:31:47 +0530

    Checking Connection

--- src/main/java/App/App1.java
+++ src/main/java/App/App1.java
@@ -3,7 +3,7 @@ package App;
 public class App1 {
     public int count(int x)
     {
-        System.out.println("Check");
+        System.out.println("Checking the Git and Github connection");
         return 10*x;
     }
 
